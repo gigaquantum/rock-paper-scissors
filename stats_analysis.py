d9b3da152b282ffd21e19c82593c13f794e9fa78
gabymol <gabymol@users.noreply.github.com>
2025-10-20 18:24:48 -0400

    Added track_win_loss_tie function

--- stats_analysis.py
+++ stats_analysis.py
@@ -8,3 +8,38 @@ Student C: Statistics & Analysis
 - Most common AI choice
 - Head-to-head results by throw type
 """
+
+## Game data stored as a list of tournaments, with each tournament being a list of dictionaries that contain the user choice, AI choice, and winner of the match
+
+# For example:
+#data = [
+#   [
+#       {"player_move": "rock", "ai_move": "paper", "winner": "ai"},
+#        {"player_move": "scissors", "ai_move": "paper", "winner": "player"},
+#        {"player_move": "scissors", "ai_move": "scissors", "winner": "none"},
+#    ],
+#    [
+#        {"player_move": "scissors", "ai_move": "rock", "winner": "ai"},
+#        {"player_move": "scissors", "ai_move": "paper", "winner": "player"},
+#        {"player_move": "rock", "ai_move": "scissors", "winner": "player"},
+#    ],
+#]
+
+#tournament is the [] and each match is {}
+
+#track win/loss/tie for each throw type
+def track_win_loss_tie(data):
+    wins = 0
+    loss = 0 
+    tie = 0 
+    for tournament in data:
+        for match in tournament:
+            if match["winner"] == "player":
+                wins += 1
+            elif match["winner"] == "ai":
+                loss += 1
+            elif match["winner"] == "none":
+                tie += 1
+    return wins, loss, tie
+
+
